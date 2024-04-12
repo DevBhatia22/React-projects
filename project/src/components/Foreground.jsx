ea3a864bfcf2a985e29b18f377b8f57c98d10171
DevBhatia22 <devbhatia222004@gmail.com>
2024-04-12 09:32:53 +0530

    fixed for mobile 1

--- project/src/components/Foreground.jsx
+++ project/src/components/Foreground.jsx
@@ -31,11 +31,11 @@ function Foreground() {
             <button className='absolute bottom-0 right-1/2 translate-x-[50%] bg-[#EEEEEE] w-20 h-10 rounded-full text-[#00ADB5] hover:bg-[#217e85] hover:text-[#EEEEEE] transition-all duration-400 mb-4' onClick={()=>{
                 adder.current.classList.toggle('hidden');
             }}> Add </button>
-            <div ref={adder} className='bottom-0 left-0 absolute w-full h-20 bg-[#00ADB5] flex items-center justify-around p-4 left- hidden'>
+            <div ref={adder} className='bottom-0 left-0 absolute w-full h-48 md:h-20 bg-[#00ADB5] flex md:flex-row flex-col gap-4 items-center justify-around p-4 left- hidden'>
                 <input value={title} className='p-1 h-2/3 border-none outline-none bg-[#EEEEEE] leading-tight -tracking-tighter text-[#00ADB5] selection:text-[#FFC470] rounded-sm w-96' placeholder='Title' onChange={(e)=>{
                     setTitle(e.target.value);
                 }}></input>
-                <input value={discription} className='p-1 h-2/3 border-none outline-none bg-[#EEEEEE] leading-tight -tracking-tighter text-[#00ADB5] selection:text-[#FFC470] rounded-sm w-[40rem]' placeholder='Discription' onChange={(e)=>{
+                <input value={discription} className='p-1 h-2/3 border-none outline-none bg-[#EEEEEE] leading-tight -tracking-tighter text-[#00ADB5] selection:text-[#FFC470] rounded-sm w-full md:w-[40rem]' placeholder='Discription' onChange={(e)=>{
                     setDiscription(e.target.value);
                 }}></input>
                 <button className='bg-[#EEEEEE] w-20 h-2/3 rounded-full text-[#00ADB5] hover:bg-[#217e85] hover:text-[#EEEEEE] transition-all duration-400' onClick={()=>{
